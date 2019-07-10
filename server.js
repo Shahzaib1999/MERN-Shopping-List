0f41de94d4f6a95c96787b30a36f5b6c2df6f1db
Shahzaib <mohdshahzaib24@gmail.com>
2019-07-11 02:13:22 +0500

    App Completed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/Items');
 
@@ -13,14 +14,26 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 // connet to mongo
-mongoose.connect(db, {useNewUrlParser: true}).then(() =>{
+mongoose.connect(db, {
+    useNewUrlParser: true
+}).then(() => {
     console.log('Mongo Connected..');
-}).catch(err =>{
+}).catch(err => {
     console.log(err);
 });
 
 // use routes
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000;
 
